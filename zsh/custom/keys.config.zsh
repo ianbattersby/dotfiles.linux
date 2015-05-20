34b359f90594860b1c75188f4a79085af9ee744d
ianbattersby <ian.battersby@gmail.com>
2015-05-20 18:05:10 +0100

    zsh: Add keybinding for incremental searching.

--- zsh/custom/keys.config.zsh
+++ zsh/custom/keys.config.zsh
@@ -2,3 +2,5 @@ bindkey -v
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 bindkey '^\' history-incremental-search-backward
+bindkey '^[Oa' history-incremental-pattern-search-backward
+bindkey '^[Ob' history-incremental-pattern-search-forward
